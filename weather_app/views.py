0997c6675096c3cd4f165aef0a31bac4933393f6
Kseniia Musagitova <kseniamusagitova@gmail.com>
2023-04-11 22:18:10 -0500

    To get weather information using a GET request, I  use an API that provides weather data

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,6 +1,21 @@
-
+import requests
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return render(request, 'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=uk&APPID=b7f7ccc381e0ac66f5bfeef48dfa5011'
+    city = 'London'
+
+    r = requests.get(url.format(city)).json()
+  
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    context = {'city_weather' : city_weather}
+
+
+    return render(request, 'weather/weather.html', context)
